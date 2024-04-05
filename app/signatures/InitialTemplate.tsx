587e6a147920b53833c60d82ff0416ff4b1a5a07
Shubham-Tiwari-Anywhere <shubham.prem@anywhere.co>
2024-04-05 19:42:12 +0530

    removed code block component and added initial template with tailwind styles

---
+++ app/signatures/InitialTemplate.tsx
@@ -0,0 +1,54 @@
+const InitialTemplate = () => {
+  return `
+  <!doctype html>
+    <html>
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        </head>
+        <body>
+            <section class="p-4">
+                <h1 class="text-3xl font-bold text-sky-600 mb-6">
+                Welcome to tailwindcss editor
+                </h1>
+                <div class="flex flex-wrap gap-4">
+                    <div class="space-y-2 rounded-xl bg-sky-400 text-white p-4">
+                        <h2>Card 1</h2>
+                        <p>This is card 1</p>
+                    </div>
+                    <div class="space-y-2 rounded-xl bg-sky-400 text-white p-4">
+                        <h2>Card 2</h2>
+                        <p>This is card 2</p>
+                    </div>
+                    <div class="space-y-2 rounded-xl bg-sky-400 text-white p-4">
+                        <h2>Card 3</h2>
+                        <p>This is card 3</p>
+                    </div>
+                    <div class="space-y-2 rounded-xl bg-sky-400 text-white p-4">
+                        <h2>Card 4</h2>
+                        <p>This is card 4</p>
+                    </div>
+                    <div class="space-y-2 rounded-xl bg-sky-400 text-white p-4">
+                        <h2>Card5</h2>
+                        <p>This is card 5</p>
+                    </div>
+                    <div class="space-y-2 rounded-xl bg-sky-400 text-white p-4">
+                        <h2>Card 6</h2>
+                        <p>This is card 6</p>
+                    </div>
+                    <div class="space-y-2 rounded-xl bg-sky-400 text-white p-4">
+                        <h2>Card 7</h2>
+                        <p>This is card 7</p>
+                    </div>
+                    <div class="space-y-2 rounded-xl bg-sky-400 text-white p-4">
+                        <h2>Card 8</h2>
+                        <p>This is card 8</p>
+                    </div>
+                </div>
+            </div>
+        </body>
+    </html>
+  `;
+};
+
+export default InitialTemplate;
